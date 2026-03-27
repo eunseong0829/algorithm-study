7b6f46289b057e031fa06370b4aa1fa444a44681
eunseong0829 <goeunseong310@gmail.com>
2026-03-27 18:14:00 +0900

    Add files via upload

---
+++ java-basic/src/calss1/ClassStart1.java
@@ -0,0 +1,15 @@
+package calss1;
+
+public class ClassStart1 {
+    static void main() {
+        String student1Name = "학생1";
+        int studentAge = 15;
+        int studentGrade = 90;
+
+        String student2Name = "학생2";
+        int student2Age = 16;
+        int student2Grade = 80;
+        System.out.println("이름:" + student1Name + "나이:" + studentAge + "성적:" + studentGrade);
+        System.out.println("이름:" + student2Name + "나이:" + student2Age + "성적:" + student2Grade);
+    }
+}
